b9e80540e0d4dc807603ddd1ac17e0a5b8fd0d8a
kellmnop <gkeller1@nd.edu>
2020-06-29 22:27:35 -0400

    Retooled torch class in immunomodeling dataset.py to work for keras/tensorflow. Implemented manual cross validation and logging of performance in keras_model.py

---
+++ immunomodeling_dataset.py
@@ -0,0 +1,80 @@
+import numpy as np
+from sklearn.model_selection import StratifiedKFold
+
+class ScoreDataset:
+	'''
+	DOC
+	'''
+	def __init__(self, features, labels, oversample=False, kfold=5):
+		assert len(features) == len(labels), "Labels (Y) and features (X) do not have the same number of observations!"
+		self.X = np.array(features).astype('float64')
+		self.Y = np.array(labels).astype('int32').reshape(len(labels),1)
+		self.leaves = []
+		self.num_leaves = 1
+		self.n_batches = 1
+		self.oversample = oversample
+		self.get_splits(kfold)
+
+	def __len__(self):
+		return len(self.X)
+
+	def __getitem__(self, i):
+		return [self.X[i], self.Y[i]]
+
+	def get_x_dims(self):
+		return self.X.shape[1]
+		
+	def get_y_dims(self):
+		return self.Y.shape[1]
+
+	def _oversample(self, X, Y):
+		'''
+		Random resampling of immunogenic peptides to equal ratio 
+		of non-immunogenic (training set only).
+		'''
+		# After working, try using SMOTE instead of random oversampling to lessen overfitting?
+		# indices of class 1
+		idx1 = [i for i in range(len(Y)) if Y[i] == 1]
+		idx0 = [i for i in range(len(Y)) if Y[i] == 0]
+		if float(sum(Y))/len(Y) < 0.5:
+			print('Oversampling class 1.')
+			# number of class 1 to resample
+			target = len(Y) - 2*len(idx1)
+			resample_idx = np.random.choice(idx1,size=target,replace=True)
+		elif float(sum(Y))/len(Y) > 0.5:
+			print('Oversampling class 0.')
+			# number of class 0 to resample
+			target = len(Y) - 2*len(idx0)
+			resample_idx = np.random.choice(idx0,size=target,replace=True)
+		else:
+			print('Class sizes already equal (no oversampling).')
+			return
+		return resample_idx
+
+	def get_splits(self, folds=5):
+		'''
+		DOC
+		'''
+		idx = np.array([i for i in range(len(self))])
+		skf = StratifiedKFold(n_splits=folds, shuffle=True)
+		assert type(folds) == int, "Folds must be type int."
+		assert folds>1, "Number of folds must be greater than 1."
+		for train_idx, val_idx in skf.split(self.X, self.Y):
+			if self.oversample:
+				train_idx = np.append(train_idx, self._oversample(self.X[train_idx], self.Y[train_idx]), axis=0)
+			self.leaves.append((train_idx, val_idx))
+		self.num_leaves = folds
+	
+	def get_training_batch(self):
+		'''
+		DOC
+		'''	
+		for leaf in self.leaves:
+			yield self.X[leaf[0]], self.Y[leaf[0]], self.X[leaf[1]], self.Y[leaf[1]]
+		
+	def get_test_set(self, leaf):
+		'''
+		
+		'''
+		idx = self.leaves[leaf][1]
+		return self.X[idx,:], self.Y[idx,:]
--- keras_model.py
+++ keras_model.py
@@ -9,32 +9,41 @@ from immunomodeling_dataset import ScoreDataset
 '''
 Implement oversampling (of training set not val) prior to training.
 '''
-
+hidden_size = 5
 
 #config = tf.ConfigProto( device_count = {'GPU': 1 , 'CPU': 12} )
 #sess = tf.Session(config=config)
 #tf.keras.backend.set_session(sess)
-callback = tf.keras.callbacks.EarlyStopping(monitor='loss', patience=6, restore_best_weights=True)
+callback = tf.keras.callbacks.EarlyStopping(monitor='loss', patience=10, restore_best_weights=True)
 
 trdata = pandas.read_csv('SBNN-feature_matrix_old.csv')
-train_X = data.iloc[:, 3:].values
-train_Y = data.iloc[:, 2].values
-train_data = ScoreDataset(train_X, train_Y, oversample=True)
+tr_X = trdata.iloc[:, 3:].values
+tr_Y = trdata.iloc[:, 2].values
 
 tedata = pandas.read_csv('SBNN-feature_matrix_old-TEST.csv')
-test_X = test_data.iloc[:, 3:].values
-test_Y = test_data.iloc[:, 2].values
-test_data = ScoreDataset(test_X, test_Y, oversample=False)
+te_X = tedata.iloc[:, 3:].values
+te_Y = tedata.iloc[:, 2].values
 
-def buildmodel():
-	model = tf.keras.models.Sequential([tf.keras.layers.Dense(5,input_shape=(81,),activation=tf.nn.relu,use_bias=True,kernel_initializer=tf.keras.initializers.glorot_normal),
+assert tr_X.shape[1] == te_X.shape[1], "Training and test data do not have the same number of features!"
+
+train_data = ScoreDataset(tr_X, tr_Y, oversample=True)
+test_data = ScoreDataset(te_X, te_Y, oversample=False)
+
+def buildmodel(n_hidden):
+	model = tf.keras.models.Sequential([tf.keras.layers.Dense(n_hidden, input_shape=(81,),activation=tf.nn.relu,use_bias=True,kernel_initializer=tf.keras.initializers.glorot_normal),
 	tf.keras.layers.Dense(1, activation=tf.nn.sigmoid, use_bias=True)])
 	model.compile(loss='binary_crossentropy',optimizer=tf.keras.optimizers.Adam(learning_rate=1e-2), metrics=[tf.keras.metrics.BinaryAccuracy(), tf.keras.metrics.AUC()])
 	return(model)
 
-estimator = KerasClassifier(build_fn=buildmodel, epochs=100, batch_size=64, verbose=1)
-kfold = RepeatedKFold(n_splits=5, n_repeats=100)
-results = cross_val_score(estimator, X, Y, cv=kfold, n_jobs=12)  # 2 cpus
-results.mean()
-
-history = model.fit(X, Y, epochs=100, batch_size=128, callbacks=[callback], shuffle=True)
+cvscores = []
+i = 0
+for train_X, train_Y, val_X, val_Y in train_data.get_training_batch():
+	model = buildmodel(hidden_size)
+	model.fit(train_X, train_Y, epochs=100, batch_size=128, callbacks=[callback], shuffle=True, verbose=0)
+	scores = model.evaluate(val_X, val_Y, verbose=0)
+	print(f'Leaf {i+1} '+', '.join([f'{model.metrics_names[i]}:{scores[i]:2f}' for i in range(len(scores))]))
+	cvscores.append(scores)
+	i += 1
+performance = np.mean(cvscores, axis=0)
+error = np.std(cvscores, axis=0)
+print(', '.join([f'{model.metrics_names[i]}: {performance[i]:2f} +/- {error[i]:2f}' for i in range(len(performance))]))
