eab4f55fad0139ac16815ea129702b8aa10e6dc1
kellmnop <gkeller1@nd.edu>
2020-07-06 18:13:30 -0400

    Minor changes to test hidden layer size.

--- keras_model.py
+++ keras_model.py
@@ -12,9 +12,9 @@ from immunomodeling_dataset import ScoreDataset
 Implement oversampling (of training set not val) prior to training.
 '''
 
-#config = tf.ConfigProto( device_count = {'GPU': 1 , 'CPU': 12} )
-#sess = tf.Session(config=config)
-#tf.keras.backend.set_session(sess)
+config = tf.ConfigProto( device_count = {'GPU': 2 , 'CPU': 8} )
+sess = tf.Session(config=config)
+tf.keras.backend.set_session(sess)
 callback = tf.keras.callbacks.EarlyStopping(monitor='loss', patience=10, restore_best_weights=True)
 
 trdata = pandas.read_csv('SBNN-feature_matrix_old.csv')
@@ -31,7 +31,7 @@ train_data = ScoreDataset(tr_X, tr_Y, oversample=True)
 test_data = ScoreDataset(te_X, te_Y, oversample=False)
 
 def buildmodel(n_hidden):
-	model = tf.keras.models.Sequential([tf.keras.layers.Dense(n_hidden, input_shape=(tr_X.shape[1],),activation=tf.nn.relu,use_bias=True,kernel_initializer=tf.keras.initializers.glorot_normal),
+	model = tf.keras.models.Sequential([tf.keras.layers.Dense(n_hidden, input_shape=(tr_X.shape[1],),activation=tf.nn.relu, use_bias=True, kernel_initializer=tf.keras.initializers.glorot_normal),
 	tf.keras.layers.Dense(1, activation=tf.nn.sigmoid, use_bias=True)])
 	model.compile(loss='binary_crossentropy', optimizer=tf.keras.optimizers.Adam(learning_rate=1e-2), metrics=[tf.keras.metrics.BinaryAccuracy(), tf.keras.metrics.AUC()])
 	return(model)
@@ -43,8 +43,10 @@ for hidden_size in h_nodes:
 	cvscores = []
 	i = 0
 	for train_X, train_Y, val_X, val_Y in train_data.get_training_batch():
+		model = None
 		model = buildmodel(hidden_size)
-		model.fit(train_X, train_Y, epochs=100, batch_size=128, callbacks=[callback], shuffle=True, verbose=0)
+		#model.fit(train_X, train_Y, epochs=100, batch_size=128, callbacks=[callback], shuffle=True, verbose=0)
+		model.fit(train_X, train_Y, epochs=100, batch_size=128, shuffle=True, verbose=0)
 		scores = model.evaluate(val_X, val_Y, verbose=0)
 		print(f'Leaf {i+1} '+', '.join([f'{model.metrics_names[i]}:{scores[i]:2f}' for i in range(len(scores))]))
 		cvscores.append(scores)
